7d7b0a448e12962ab3e33b7105c380c15c62f2f2
NikitaKS <pancxxx2@gmail.com>
2020-01-18 15:08:39 +0100

    component did update

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
-  render() {
+    componentDidUpdate() {
+        console.log('ComponentDidUpdate');
+    }
+
+    render() {
     return (
         <div className="App">
           <header className="App-header">
