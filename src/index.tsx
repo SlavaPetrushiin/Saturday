84bd5b27f783768fc80e2cae31bb53f3f22bee2e
Slava <slava91petrushin@yandex.ru>
2020-01-18 17:40:35 +0300

    added setInterval rerender

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rerenderApp = () => {
+    ReactDOM.render(<App />, document.getElementById('root'));
+}
+
+rerenderApp();
+
+setInterval(rerenderApp, 5000);
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
